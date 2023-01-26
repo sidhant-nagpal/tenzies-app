ec82302c7ff8978344db390f2b837539bfd9f783
Sidhant Nagpal <sidnagpal2000@gmail.com>
2023-01-26 21:18:26 +0530

    init

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Die from "./Die"
+
+export default function App() {
+    return (
+        <main className="dice-container">
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+        </main>
+    )
+}
